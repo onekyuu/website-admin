adf97f93bed0ff54e65cc5a10c7a19a86154eff3
onekyuu <me@keyu.email>
2025-04-20 03:39:50 +0800

    fix: add staticfile dir

--- backend/blog/settings.py
+++ backend/blog/settings.py
@@ -16,6 +16,7 @@ from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
