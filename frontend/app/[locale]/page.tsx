25baaeb66e61f9f719221912b4030caf2c2bbe99
onekyuu <me@keyu.email>
2025-04-20 17:14:49 +0800

    fix: [frontend]remove never used code

--- frontend/app/[locale]/page.tsx
+++ frontend/app/[locale]/page.tsx
@@ -3,12 +3,9 @@
 import LocaleSwitcher from "@/components/LocaleSwitcher";
 import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
-import { usePathname, useRouter } from "next/navigation";
 
 export default function Home() {
   const t = useTranslations("login");
-  const router = useRouter();
-  const pathname = usePathname();
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
