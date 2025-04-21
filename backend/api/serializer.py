28912b1efca65d648a1b51bfc2313231f44cf8ce
onekyuu <me@keyu.email>
2025-04-21 20:48:29 +0800

    feat: [backend] add serializers

---
+++ backend/api/serializer.py
@@ -0,0 +1,130 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+
+from api import models as api_models
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['full_name'] = user.full_name
+        token['email'] = user.email
+        token['username'] = user.username
+        return token
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password])
+    confirm_password = serializers.CharField(
+        write_only=True, required=True, style={'input_type': 'password'})
+
+    class Meta:
+        model = api_models.User
+        fields = ['full_name', 'username', 'email',
+                  'password', 'confirm_password']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        user = api_models.User(
+            email=validated_data['email'],
+            full_name=validated_data['full_name']
+        )
+        email_username = user.email.split('@')
+        user.username = email_username
+        user.set_password(validate_password['password'])
+        user.save()
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = api_models.User
+        fields = "__all__"
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = api_models.Profile
+        fields = "__all__"
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    def get_post_count(self, category):
+        return category.posts.count()
+
+    class Meta:
+        model = api_models.Category
+        fields = ["id", "title", "image", "slug", "post_count"]
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = api_models.Comment
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(CommentSerializer).__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = api_models.Post
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(PostSerializer).__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+
+class BookmarkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = api_models.Bookmark
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(BookmarkSerializer).__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = api_models.Notification
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(NotificationSerializer).__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+
+class AuthorSerializer(serializers.Serializer):
+    views = serializers.IntegerField(default=0)
+    posts = serializers.IntegerField(default=0)
+    likes = serializers.IntegerField(default=0)
+    comments = serializers.IntegerField(default=0)
+    bookmarks = serializers.IntegerField(default=0)
