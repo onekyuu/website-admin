dc6fec2c8029bf2157a41a2f7c2530586838cefb
onekyuu <me@keyu.email>
2025-04-23 12:08:58 +0800

    feat: [frontend] update dark-theme

--- frontend/components/ModeToggle.tsx
+++ frontend/components/ModeToggle.tsx
@@ -5,18 +5,23 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme();
 
+  useEffect(() => {
+    const localStorageTheme = localStorage.getItem("theme");
+    setTheme(localStorageTheme === "dark" ? "dark" : "light");
+  }, [setTheme]);
+
   return (
     <>
-      {theme === "light" && (
+      {theme === "light" ? (
         <Button variant="outline" size="icon" onClick={() => setTheme("dark")}>
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         </Button>
-      )}
-      {theme === "dark" && (
+      ) : (
         <Button variant="outline" size="icon" onClick={() => setTheme("light")}>
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         </Button>
