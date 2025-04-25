a2c5443e61e59bfb79c55a98f796e334b6e07109
onekyuu <me@keyu.email>
2025-04-25 18:29:22 +0800

    feat: [frontend] add ts type check in pre-commit

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,8 +1,6 @@
 import { FC } from "react";
 
-interface HeaderProps {}
-
-const Header: FC<HeaderProps> = () => {
+const Header: FC = () => {
   return (
     <div className="h-18 w-full bg-gray-800 text-white flex items-center justify-center">
       header
