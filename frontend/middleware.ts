382396374345f91ed7d9dd40fde7667379e07e3a
onekyuu <me@keyu.email>
2025-04-26 02:30:38 +0800

    feat: [frontend] update navigator

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,26 +1,37 @@
 import createMiddleware from "next-intl/middleware";
 import { NextRequest, NextResponse } from "next/server";
 
+const SUPPORTED_LOCALES: string[] = ["zh", "en", "ja"];
+
 const intlMiddleware = createMiddleware({
-  locales: ["zh", "en", "ja"],
-  defaultLocale: "zh",
+  locales: SUPPORTED_LOCALES,
+  defaultLocale: "en",
   localeDetection: true,
 });
 
-export const locales = ["en", "zh", "ja"] as const;
-
 const publicPages = ["/login"];
 
+function detectLocaleFromAcceptLanguage(acceptLanguage: string | null): string {
+  if (!acceptLanguage) return "zh"; // 默认语言
+  const language = acceptLanguage.split(",")[0].split("-")[0]; // 提取主语言部分
+  return SUPPORTED_LOCALES.includes(language) ? language : "en";
+}
+
 export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  if (pathname === "/") {
+    const acceptLanguage = request.headers.get("accept-language");
+    const locale = detectLocaleFromAcceptLanguage(acceptLanguage);
+    return NextResponse.redirect(new URL(`/${locale}`, request.url));
+  }
   const publicPathnameRegex = RegExp(
-    `^(/(${locales.join("|")}))?(${publicPages
+    `^(/(${SUPPORTED_LOCALES.join("|")}))?(${publicPages
       .flatMap((p) => (p === "/" ? ["", "/"] : p))
       .join("|")})/?$`,
     "i",
   );
   const isPublicPage = publicPathnameRegex.test(request.nextUrl.pathname);
 
-  const { pathname } = request.nextUrl;
   const localeMatch = pathname.match(/^\/(zh|en|ja)(\/|$)/);
   const localePrefix = localeMatch?.[0] || "";
   if (isPublicPage) {
