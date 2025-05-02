59c53b9cb838029f7048f50f4599549869f4d129
onekyuu <me@keyu.email>
2025-05-02 17:46:09 +0800

    fix: [frontend] metadata

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -12,33 +12,27 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "./ui/breadcrumb";
-import useUserData from "@/hooks/useUserData";
 
 const Header: FC = () => {
-  const user_id = useUserData()?.user_id;
   return (
     <div className="h-16 w-full flex items-center justify-between pr-8">
-      {user_id ? (
-        <div className="flex items-center">
-          <SidebarTrigger />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </div>
-      ) : (
-        <div />
-      )}
+      <div className="flex items-center">
+        <SidebarTrigger />
+        <Separator orientation="vertical" className="mr-2 h-4" />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem className="hidden md:block">
+              <BreadcrumbLink href="#">
+                Building Your Application
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator className="hidden md:block" />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
       <div className="flex w-max gap-4">
         <LocaleSwitcher />
         <ModeToggle />
