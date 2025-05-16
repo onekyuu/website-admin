256eed1ec0c4193dc91ca90c6394aa6b17b06a1d
onekyuu <me@keyu.email>
2025-05-16 22:46:33 +0800

    feat: [frontend] add Post Form for edit

--- frontend/app/(main)/[locale]/posts/[slug]/edit/page.tsx
+++ frontend/app/(main)/[locale]/posts/[slug]/edit/page.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+"use client";
+
+import { get } from "@/lib/fetcher";
+import { useQuery } from "@tanstack/react-query";
+import React, { useMemo } from "react";
+import { PostData } from "../../page";
+import { useParams } from "next/navigation";
+import PostForm, { LanguageCode, Post } from "@/components/PostForm";
+import useUserData from "@/hooks/useUserData";
 
 const PostEditPage = () => {
-  return <div>PostEditPage</div>;
+  const params = useParams();
+  const slug = params?.slug;
+  const locale = params?.locale as LanguageCode;
+  const userId = useUserData()?.user_id;
+  const { data } = useQuery({
+    queryKey: ["post-detail", slug],
+    queryFn: () => get<PostData>(`/post/detail/${slug}/`),
+  });
+
+  const initalValues = useMemo(() => {
+    const translations = data?.translations;
+    const defaultLanguage = locale || "zh";
+    const translation = translations?.find(
+      (item) => item.language === defaultLanguage,
+    );
+    return {
+      ...data,
+      ...translations,
+      user_id: Number(userId),
+      category: data?.category.id,
+    };
+  }, [data, locale, userId]);
+
+  console.log("data", data);
+
+  const handleSubmit = async (data: Post) => {};
+  return (
+    <div>
+      <PostForm
+        initialValues={initalValues}
+        userId={Number(userId)}
+        onSubmit={handleSubmit}
+        defaltLanguage={locale}
+      />
+    </div>
+  );
 };
 
 export default PostEditPage;
