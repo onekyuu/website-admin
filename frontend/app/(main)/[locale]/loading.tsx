b1dda005a78a08910f6927c89e9da713aef827e8
onekyuu <me@keyu.email>
2025-05-24 16:45:25 +0800

    feat: [frontend] update ui

--- frontend/app/(main)/[locale]/loading.tsx
+++ frontend/app/(main)/[locale]/loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div>
+    <div className="h-full w-full flex items-center justify-center">
       <Loader className="animate-spin" />
     </div>
   );
