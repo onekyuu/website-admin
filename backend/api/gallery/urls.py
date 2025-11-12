3c575f1ce950a644b678603262a5a0cdc8c9dbb6
onekyuu <me@keyu.email>
2025-11-12 22:39:47 +0800

    [backend]feat: add gallery timeline api

--- backend/api/gallery/urls.py
+++ backend/api/gallery/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('gallery/create/', views.GalleryCreateView.as_view(), name='gallery-create'),
     path('gallery/detail/<slug:slug>/', views.GalleryDetailView.as_view(),
          name='gallery-detail'),
+    path('gallery/timeline/', views.GalleryTimelineView.as_view()),
+    path('gallery/timeline/<int:year>/', views.GalleryYearView.as_view()),
 ]
