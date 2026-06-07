fa010777a9d44a8b3861e07c0768aef6d9e53273
onekyuu <me@keyu.email>
2026-06-07 11:49:41 +0800

    feat: self-host frontend fonts

--- frontend/app/(auth)/[locale]/login/layout.tsx
+++ frontend/app/(auth)/[locale]/login/layout.tsx
@@ -2,8 +2,6 @@ import { notFound } from "next/navigation";
 import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { ReactNode } from "react";
 import { routing } from "@/i18n/config";
-import { Roboto, Noto_Sans_JP, Noto_Sans_SC } from "next/font/google";
-import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ReactQueryProvider } from "@/lib/react-query";
 import { Toaster } from "@/components/ui/sonner";
@@ -13,21 +11,6 @@ type Props = {
   params: Promise<{ locale: string }>;
 };
 
-const roboto = Roboto({
-  variable: "--font-roboto",
-  subsets: ["latin"],
-});
-
-const notoSansJP = Noto_Sans_JP({
-  variable: "--font-noto-sans-jp",
-  subsets: ["latin"],
-});
-
-const notoSansSC = Noto_Sans_SC({
-  variable: "--font-noto-sans-sc",
-  subsets: ["latin"],
-});
-
 export default async function LocaleLayout({ children, params }: Props) {
   // Ensure that the incoming `locale` is valid
   const { locale } = await params;
@@ -36,14 +19,14 @@ export default async function LocaleLayout({ children, params }: Props) {
   }
 
   const fonts: { [K: string]: string } = {
-    en: roboto.className,
-    zh: notoSansSC.className,
-    ja: notoSansJP.className,
+    en: "font-roboto",
+    zh: "font-noto-sans-sc",
+    ja: "font-noto-sans-jp",
   };
 
   return (
     <html lang={locale}>
-      <body className={cn(fonts[locale])}>
+      <body className={fonts[locale]}>
         <ReactQueryProvider>
           <ThemeProvider
             attribute="class"
